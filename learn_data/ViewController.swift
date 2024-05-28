e49c7bbb8fdcdd738f41ee27ae18fe502e9b7244
HirotoTaniguchi-genero <hiroto.taniguchi@generosity.co.jp>
2024-05-28 15:06:22 +0900

    Initial Commit

---
+++ learn_data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  learn_data
+//
+//  Created by SnSnap1159 on 2024/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
